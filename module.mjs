b5e2b0c3d6c5e77227817b1e0603e2ccc0f47b18
zaneobelisco-ctrl <zaneobelisco@gmail.com>
2025-11-09 16:26:57 -0300

    Update module.mjs

--- module.mjs
+++ module.mjs
@@ -2,9 +2,10 @@ import { DispatchActorSheet } from "./module/actor-sheet.js";
 
 Hooks.once("init", function() {
   console.log("Dispatch RPG | Inicializando sistema...");
-
   Actors.unregisterSheet("core", ActorSheet);
+  // registra a ficha PARA o tipo "character"
   Actors.registerSheet("dispatchrpg", DispatchActorSheet, {
-    makeDefault: true
+    makeDefault: true,
+    types: ["character"]
   });
 });
